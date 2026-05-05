fa0424849ffce5e0fef68890572398434ec3ad5d
Elangkavi <elangkavik3@gmail.com>
2026-05-05 15:42:59 +0530

    api update

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:8080";
+  import.meta.env.VITE_API_URL || "https://nalvel-billing-1.onrender.com/";
 
 export default API_BASE_URL;
