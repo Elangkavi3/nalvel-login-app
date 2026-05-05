ea772431efbb006d6f244fead3d56a6291d0827d
Elangkavi <elangkavik3@gmail.com>
2026-05-05 15:51:05 +0530

    api update1

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import API_BASE_URL from "../config/api";
 
 export async function loginUser(email, password) {
-  const res = await fetch(`${API_BASE_URL}/api/auth/login`, {
+  const res = await fetch(`${API_BASE_URL}api/auth/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
